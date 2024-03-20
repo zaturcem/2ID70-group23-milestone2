24fe86470e9fc9c6f18d77645326ad3dbcb46bd7
zaturcem <cem.namoglu@gmail.com>
2024-03-20 14:01:17 +0100

    Initial commit skeleton files

---
+++ Readme.txt
@@ -0,0 +1,11 @@
+This is a template project for team project of 2ID70 2024.
+
+Various comments in the main.py file indicate where functionality needs to be implemented. These comments are marked TODO.
+
+You are allowed to change main.py only at the places where it is clearly indicated (with the TODO comments). You should not change the code in other places. You can, however, add new functions to keep your code better organized.
+
+Do make sure that there exists a file called "main.py" which contains the 'main module statement' of the form "if __name__ == '__main__':".
+
+You can ZIP the main.py file using any common compression tool. The archive should be called app.zip and can be uploaded as such to the server. Ensure that the archive **does not** contain folders!
+
+Good luck on the project!
---
+++ main.py
@@ -0,0 +1,72 @@
+from pyspark import SparkConf, SparkContext, RDD
+from pyspark.sql import SparkSession
+
+
+# The code below may help you if your pc cannot find the correct python executable.
+# Don't use this code on the server!
+# import os
+# import sys
+# os.environ['PYSPARK_PYTHON'] = sys.executable
+# os.environ['PYSPARK_DRIVER_PYTHON'] = sys.executable
+
+# TODO: Make sure that if you installed Spark version 3.3.4 (recommended) that you install the same version of
+#  PySpark. You can do this by running the following command: pip install pyspark==3.3.4
+
+
+def get_spark_context(on_server: bool) -> SparkContext:
+    spark_conf = SparkConf().setAppName("2ID70")
+    if not on_server:
+        spark_conf = spark_conf.setMaster("local[*]")
+
+    spark_context = SparkContext.getOrCreate(spark_conf)
+
+    if on_server:
+        # TODO: You may want to change ERROR to WARN to receive more info. For larger data sets, to not set the
+        #  log level to anything below WARN, Spark will print too much information.
+        spark_context.setLogLevel("ERROR")
+
+    return spark_context
+
+
+def q1(spark_context: SparkContext, on_server: bool) -> (RDD, RDD):
+    events_file_path = "/events.csv" if on_server else "events.csv"
+    event_types_file_path = "/event_types.csv" if on_server else "event_types.csv"
+
+    # TODO: Implement Q1 here
+
+    cleaned_events = None
+    cleaned_event_types = None
+
+    return cleaned_events, cleaned_event_types
+
+
+def q2(spark_context: SparkContext, events: RDD, event_types: RDD):
+    spark_session = SparkSession(spark_context)
+    # TODO: Implement Q2 here
+    pass
+
+
+def q3(spark_context: SparkContext, events: RDD, event_types: RDD):
+    # TODO: Implement Q3 here
+    pass
+
+
+def q4(spark_context: SparkContext, events: RDD, event_types: RDD):
+    # TODO: Implement Q4 here
+    return
+
+
+if __name__ == '__main__':
+    on_server = False  # TODO: Set this to true if and only if deploying to the server
+
+    spark_context = get_spark_context(on_server)
+
+    cleaned_events, cleaned_event_types = q1(spark_context, on_server)
+
+    q2(spark_context, cleaned_events, cleaned_event_types)
+
+    q3(spark_context, cleaned_events, cleaned_event_types)
+
+    q4(spark_context, cleaned_events, cleaned_event_types)
+
+    spark_context.stop()
