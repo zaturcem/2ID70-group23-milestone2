12d0ea99dd9d7cd829e8c6f6e3b025db3d4f8294
TrenBeelen <125546933+TrenBeelen@users.noreply.github.com>
2024-03-27 15:44:47 +0100

    Q3 1591 dictionary count

--- main.py
+++ main.py
@@ -45,7 +45,10 @@ def q1(spark_context: SparkContext, on_server: bool) -> (RDD, RDD):
     cleaned_events = event_RDD_filtered.collect()
     cleaned_event_types = event_type_RDD_filtered.collect()
 
-    print(f""">>[q11: {event_RDD_filtered.count()}]\n>>[q12: {event_type_RDD_filtered.count()}]\n>>[q13: {event_RDD.count() - event_RDD_filtered.count()}]\n>>[q14: {event_type_RDD.count() - event_type_RDD_filtered.count()}]""")
+    print(f'[q11: {event_RDD_filtered.count()}]')
+    print(f'[q12: {event_type_RDD_filtered.count()}]')
+    print(f'[q13: {event_RDD.count() - event_RDD_filtered.count()}]')
+    print(f'[q14: {event_type_RDD.count() - event_type_RDD_filtered.count()}]')
     
     return cleaned_events, cleaned_event_types
 
@@ -58,8 +61,64 @@ def q2(spark_context: SparkContext, events: RDD, event_types: RDD):
 
 def q3(spark_context: SparkContext, events: RDD, event_types: RDD):
     # TODO: Implement Q3 here
-    pass
 
+    #Clean original data according to question 1
+    events_file_path = "/events.csv" if on_server else "events.csv"
+    event_types_file_path = "/event_types.csv" if on_server else "event_types.csv"
+
+    event_RDD = spark_context.textFile(events_file_path)
+    event_type_RDD = spark_context.textFile(event_types_file_path)
+
+    event_RDD_filtered = event_RDD.filter(
+        lambda line: len(line.split(',')) == 3 and all(map(lambda x: x.isdigit(), line.split(','))))
+    event_type_RDD_filtered = event_type_RDD.filter(
+        lambda line: len(line.split(',')) == 2 and all(map(lambda x: x.isdigit(), line.split(','))))
+
+
+    #Q3 solution
+
+    event_RDD_split = event_RDD_filtered.map(lambda x: x.split(","))
+
+    count=0
+
+    for i in range(0,5):
+        event_RDD_series = event_RDD_split.filter(lambda x : x[0]==str(i))
+
+        event_RDD_sortby_4 = event_RDD_series.sortBy(lambda x : x[1])
+
+        #print("Number of distinct identified frequent event sequences of size 3:", event_RDD_sortby_4.collect())
+
+        test = event_RDD_sortby_4.collect()
+        a = 0
+        b = 3
+        sequence_list = []
+        for i in range(0, len(test)-2):
+            #print(i)
+            seq = test[a:b] 
+            #print(seq)
+            sequence = [seq[0][2], seq[1][2], seq[2][2]]
+            sequence_list.append(str(sequence))
+            a+=1
+            b+=1 
+
+
+        sequence_list
+
+        frequency = {}
+        for item in sequence_list:
+            if (item in frequency):
+                frequency[item] += 1
+            else:
+                frequency[item] = 1
+
+        #print(frequency)
+
+        
+        for value in frequency.values():
+            if value >= 5:
+                count += 1
+
+    print("Number of times the value is 5 or higher:", count)
 
 def q4(spark_context: SparkContext, events: RDD, event_types: RDD):
     # TODO: Implement Q4 here
